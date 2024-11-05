0848899ddd3d5442209e792c4da06f52df324b66
Marcus Birke <marcus.birke@gmail.com>
2024-11-05 16:28:21 +0100

    reverted to yesterday without layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <h1>Welcome to the Endurance Training App!</h1>;
+  return <h1>Welcome to the Endurance Training App! with a Test</h1>;
 };
 
 export default Home;
